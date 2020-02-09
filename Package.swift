20c82ba5e94d3347acffc4b074d6a4e53fcb17c5
Gesen <i@gesen.me>
2020-02-09 15:39:06 +0800

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GSPlayer",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "GSPlayer",
+            targets: ["GSPlayer"]),
+    ],
+    targets: [
+        .target(
+            name: "GSPlayer",
+            path: "GSPlayer/Classes"),
+    ]
+)
