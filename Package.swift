3ee1a851dda94aec7c257599f6b990de742a725b
Chirag <chiragshenoy@gmail.com>
2021-04-23 17:43:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GSPlayer",
-    platforms: [.iOS(.v11)]
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "GSPlayer",
