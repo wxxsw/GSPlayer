e1d3b45e652611bc344c2db44ac92de1d61a7983
Dmitry Kozlov <d@kozlov.ru>
2021-07-15 20:52:38 +0300

    Swift package manager: Reduced iOS version to 10

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GSPlayer",
-    platforms: [.iOS(.v13),
+    platforms: [.iOS(.v10),
                 .macOS(.v10_12)],
     products: [
         .library(
